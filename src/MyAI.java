2ee799953ff499cc9a6b94a5d6acd84b366af7d4
Kastin <271kastine@student.dist113.org>
2026-09-17 10:42:34 -0500

    Worked on attacking oscilators and still lives

--- src/MyAI.java
+++ src/MyAI.java
@@ -1,8 +1,8 @@
 /**
  * STUDENT FILE
  *
- * Name: ______________________________
- * AI Code Name: ______________________
+ * Name: Elliott Kastin
+ * AI Code Name: ChipAI
  *
  * Strategy Description:
  * Replace this comment with a short explanation of the strategy your AI uses.
@@ -12,7 +12,7 @@ public class MyAI extends CellAI {
 
     @Override
     public String getAIName() {
-        return "MyAI - CHANGE ME";
+        return "MyAI - ChipAI";
     }
 
     @Override
@@ -29,6 +29,54 @@ public class MyAI extends CellAI {
          *   GridFunctions.mostCommonNeighbor -> most common neighboring AI
          *   randomInt(bound)            -> reproducible random integer
          */
+        int MyID = getID();
+        int[][] Ids = new int[grid.getRows()][grid.getCols()];
+        for (int r = 0; r < grid.getRows(); r++) {
+            for (int c = 0; c < grid.getCols(); c++) {
+                    Ids[r][c] = grid.getCell(r, c);
+                }
+            }
+        //targets a 3 in a row ocilator
+        for(int r = 0; r < grid.getRows(); r++) {
+            for (int c = 0; c < grid.getCols(); c++) {
+                if (Ids[r][c] != MyID && Ids[r][c] != -1) {
+                    int neighbors = GridFunctions.getNeighbors(r, c, grid);
+                    if (neighbors == 2) {
+                        if(GridFunctions.mostCommonNeighbor(r, c, grid) != MyID) {
+                            if(r-1 >= 0 && r+1 < grid.getRows()) {
+                                if(Ids[r-1][c] != -1 && Ids[r+1][c] != -1 && Ids[r-1][c] != MyID && Ids[r+1][c] != MyID) {
+                                    return new Location(r, c);
+                                }
+                                else if(c-1 >= 0 && c+1 < grid.getCols()) 
+                                {
+                                    if(Ids[r][c-1] != -1 && Ids[r][c+1] != -1 && Ids[r][c-1] != MyID && Ids[r][c+1] != MyID) {
+                                        return new Location(r, c);
+                                    }
+                                }
+                            }
+                            else if(c-1 >= 0 && c+1 < grid.getCols()) 
+                            {
+                                if(Ids[r][c-1] != -1 && Ids[r][c+1] != -1 && Ids[r][c-1] != MyID && Ids[r][c+1] != MyID) {
+                                    return new Location(r, c);
+                                }
+                            }
+                        }
+                    }
+                    else if(neighbors == 3) {
+                        if(GridFunctions.mostCommonNeighbor(r, c, grid) != MyID) {
+                            if(r-1 >= 0 && r+1 < grid.getRows() && c-1 >= 0 && c+1 < grid.getCols()) {
+                                if(Ids[r-1][c] != -1 && Ids[r+1][c] == -1){
+                                   if(Ids[r][c + 1] != -1) {
+                                        return new Location(r+2, c);
+                                    }
+                                    return new Location(r-2, c);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return new Location(randomInt(grid.getRows()), randomInt(grid.getCols()));
     }
 }
