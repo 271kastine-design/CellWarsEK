81a1e61115a511f148c64362045b609b26bce5e1
Elliott Kastin <elliottkastin@MacBook-Air-4.local>
2026-09-20 21:57:50 -0500

    Added sideways variation of stilllife

--- src/MyAIStillLife.java
+++ src/MyAIStillLife.java
@@ -62,6 +62,30 @@ public class MyAIStillLife {
                             }
                         }
                     }
+                    if (r + 2 < grid.getRows() && c + 3 < grid.getCols() && c - 1 >= 0) {
+                        boolean sidewaysBracketPattern =
+                                ids[r][c - 1] == -1
+                                && ids[r][c] == -1
+                                && ids[r][c + 1] != -1
+                                && ids[r][c + 2] != -1
+                                && ids[r][c + 3] == -1
+
+                                && ids[r + 1][c - 1] == -1
+                                && ids[r + 1][c] != -1
+                                && ids[r + 1][c + 1] == -1
+                                && ids[r + 1][c + 2] == -1
+                                && ids[r + 1][c + 3] != -1
+
+                                && ids[r + 2][c - 1] == -1
+                                && ids[r + 2][c] == -1
+                                && ids[r + 2][c + 1] != -1
+                                && ids[r + 2][c + 2] != -1
+                                && ids[r + 2][c + 3] == -1;
+
+                        if (sidewaysBracketPattern) {
+                            return new Location(r, c + 1);
+                        }
+                    }
                 }
             }
         }
