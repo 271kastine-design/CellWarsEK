ae36a9ad65fd8c6ed6c65abbe6fc3cc9e65fefbc
Kastin <271kastine@student.dist113.org>
2026-09-22 09:35:31 -0500

    More refinements

--- src/MyAIDefense.java
+++ src/MyAIDefense.java
@@ -19,10 +19,7 @@ public class MyAIDefense {
             for (int c = 0; c < grid.getCols(); c++) {
                 if (grid.getCell(r, c) == myIDLocal) {
                     int neighbors = GridFunctions.getNeighbors(r, c, grid);
-                    /* - x -
-                       x - x
-                       x - x
-                       - x - */
+
                     if (neighbors == 2 && GridFunctions.mostCommonNeighbor(r, c, grid) == myIDLocal) {
                         if (r + 3 < grid.getRows() && c - 2 >= 0 && c + 2 < grid.getCols()) {
                             boolean bracketPattern = ids[r][c] != -1
@@ -30,34 +27,32 @@ public class MyAIDefense {
                                     && ids[r + 1][c] == -1 && ids[r + 1][c - 1] != -1 && ids[r + 1][c + 1] != -1
                                     && ids[r + 2][c] == -1 && ids[r + 2][c - 1] != -1 && ids[r + 2][c + 1] != -1
                                     && ids[r + 3][c] != -1 && ids[r + 3][c - 1] == -1 && ids[r + 3][c + 1] == -1
-                                    && ids[r][c - 2] == -1 && ids[r + 1][c - 2] == -1 && ids[r + 2][c - 2] == -1 && ids[r + 3][c - 2] == -1
-                                    && ids[r][c + 2] == -1 && ids[r + 1][c + 2] == -1 && ids[r + 2][c + 2] == -1 && ids[r + 3][c + 2] == -1;
+                                    && ids[r][c - 2] == -1 && ids[r + 1][c - 2] == -1
+                                    && ids[r + 2][c - 2] == -1 && ids[r + 3][c - 2] == -1
+                                    && ids[r][c + 2] == -1 && ids[r + 1][c + 2] == -1
+                                    && ids[r + 2][c + 2] == -1 && ids[r + 3][c + 2] == -1;
 
                             if (bracketPattern) {
                                 return new Location(r + 1, c);
                             }
                         }
-                        /* - x x -
-                           x - - x
-                           - x x - */
-                        if (r + 2 < grid.getRows() && c + 3 < grid.getCols() && c - 1 >= 0) {
+
+                        if (r + 2 < grid.getRows() && c - 1 >= 0 && c + 3 < grid.getCols()) {
                             boolean sidewaysBracketPattern =
                                     ids[r][c - 1] == -1
-                                    && ids[r][c] == -1
+                                    && ids[r][c] != -1
                                     && ids[r][c + 1] != -1
-                                    && ids[r][c + 2] != -1
+                                    && ids[r][c + 2] == -1
                                     && ids[r][c + 3] == -1
-
                                     && ids[r + 1][c - 1] == -1
-                                    && ids[r + 1][c] != -1
+                                    && ids[r + 1][c] == -1
                                     && ids[r + 1][c + 1] == -1
-                                    && ids[r + 1][c + 2] == -1
-                                    && ids[r + 1][c + 3] != -1
-
+                                    && ids[r + 1][c + 2] != -1
+                                    && ids[r + 1][c + 3] == -1
                                     && ids[r + 2][c - 1] == -1
-                                    && ids[r + 2][c] == -1
+                                    && ids[r + 2][c] != -1
                                     && ids[r + 2][c + 1] != -1
-                                    && ids[r + 2][c + 2] != -1
+                                    && ids[r + 2][c + 2] == -1
                                     && ids[r + 2][c + 3] == -1;
 
                             if (sidewaysBracketPattern) {
@@ -66,78 +61,47 @@ public class MyAIDefense {
                         }
                     }
 
-                    if (neighbors == 3 && GridFunctions.mostCommonNeighbor(r, c, grid) == myIDLocal) {
-                        if (r - 1 >= 0 && r + 3 < grid.getRows()
-                            && c - 1 >= 0 && c + 3 < grid.getCols()) {
+                }
+            }
+        }
+        for (int r = 0; r < grid.getRows(); r++) {
+            for (int c = 0; c < grid.getCols(); c++) {
+                if (r >= 1 && r + 3 < grid.getRows()
+                        && c >= 1 && c + 3 < grid.getCols()) {
+                    boolean boatPattern =
+                            ids[r - 1][c - 1] == -1
+                            && ids[r - 1][c] == -1
+                            && ids[r - 1][c + 1] == -1
+                            && ids[r - 1][c + 2] == -1
+                            && ids[r - 1][c + 3] == -1
+                            && ids[r][c - 1] == -1
+                            && ids[r][c] == -1
+                            && ids[r][c + 1] != -1
+                            && ids[r][c + 2] != -1
+                            && ids[r][c + 3] == -1
+                            && ids[r + 1][c - 1] == -1
+                            && ids[r + 1][c] != -1
+                            && ids[r + 1][c + 1] == -1
+                            && ids[r + 1][c + 2] != -1
+                            && ids[r + 1][c + 3] == -1
+                            && ids[r + 2][c - 1] == -1
+                            && ids[r + 2][c] == -1
+                            && ids[r + 2][c + 1] != -1
+                            && ids[r + 2][c + 2] == -1
+                            && ids[r + 2][c + 3] == -1
+                            && ids[r + 3][c - 1] == -1
+                            && ids[r + 3][c] == -1
+                            && ids[r + 3][c + 1] == -1
+                            && ids[r + 3][c + 2] == -1
+                            && ids[r + 3][c + 3] == -1;
 
-                            boolean boatPattern =
-                                // empty border above
-                                ids[r - 1][c - 1] == -1
-                                && ids[r - 1][c] == -1
-                                && ids[r - 1][c + 1] == -1
-                                && ids[r - 1][c + 2] == -1
-                                && ids[r - 1][c + 3] == -1
-
-                                // pattern row 1
-                                && ids[r][c - 1] == -1
-                                && ids[r][c] == -1
-                                && ids[r][c + 1] != -1
-                                && ids[r][c + 2] != -1
-                                && ids[r][c + 3] == -1
-
-                                // pattern row 2
-                                && ids[r + 1][c - 1] == -1
-                                && ids[r + 1][c] != -1
-                                && ids[r + 1][c + 1] == -1
-                                && ids[r + 1][c + 2] != -1
-                                && ids[r + 1][c + 3] == -1
-
-                                // pattern row 3
-                                && ids[r + 2][c - 1] == -1
-                                && ids[r + 2][c] == -1
-                                && ids[r + 2][c + 1] != -1
-                                && ids[r + 2][c + 2] == -1
-                                && ids[r + 2][c + 3] == -1
-
-                                // empty border below
-                                && ids[r + 3][c - 1] == -1
-                                && ids[r + 3][c] == -1
-                                && ids[r + 3][c + 1] == -1
-                                && ids[r + 3][c + 2] == -1
-                                && ids[r + 3][c + 3] == -1;
-
-                            if (boatPattern) {
-                                return new Location(r + 1, c);
-                            }
-                        }if (r - 1 >= 0 && r + 2 < grid.getRows()
-                                && c - 1 >= 0 && c + 2 < grid.getCols()) {
-
-                            boolean stillLife =
-                                ids[r - 1][c - 1] == -1 &&
-                                ids[r - 1][c] == -1 &&
-                                ids[r - 1][c + 1] == -1 &&
-                                ids[r - 1][c + 2] == -1 &&
-                                ids[r][c - 1] == -1 &&
-                                ids[r][c] != -1 &&
-                                ids[r][c + 1] != -1 &&
-                                ids[r][c + 2] == -1 &&
-                                ids[r + 1][c - 1] == -1 &&
-                                ids[r + 1][c] != -1 &&
-                                ids[r + 1][c + 1] != -1 &&
-                                ids[r + 1][c + 2] == -1 &&
-                                ids[r + 2][c - 1] == -1 &&
-                                ids[r + 2][c] == -1 &&
-                                ids[r + 2][c + 1] == -1 &&
-                                ids[r + 2][c + 2] == -1;
-
-                            if (stillLife) {
-                                return new Location(r, c - 1);
-                            }
-                        }
+                    if (boatPattern) {
+                        return new Location(r + 1, c);
                     }
                 }
             }
         }
+
         return null;
     }
 }
--- src/MyAIOffense.java
+++ src/MyAIOffense.java
@@ -14,55 +14,51 @@ public final class MyAIOffense {
                 ids[r][c] = grid.getCell(r, c);
             }
         }
+
+        for (int r = 0; r < grid.getRows(); r++) {
+            for (int c = 0; c < grid.getCols(); c++) {
+                if (r >= 1 && r + 3 < grid.getRows()
+                        && c >= 1 && c + 3 < grid.getCols()) {
+                    boolean boatPattern =
+                            ids[r - 1][c - 1] == -1
+                            && ids[r - 1][c] == -1
+                            && ids[r - 1][c + 1] == -1
+                            && ids[r - 1][c + 2] == -1
+                            && ids[r - 1][c + 3] == -1
+                            && ids[r][c - 1] == -1
+                            && ids[r][c] == -1
+                            && ids[r][c + 1] != -1
+                            && ids[r][c + 2] != -1
+                            && ids[r][c + 3] == -1
+                            && ids[r + 1][c - 1] == -1
+                            && ids[r + 1][c] != -1
+                            && ids[r + 1][c + 1] == -1
+                            && ids[r + 1][c + 2] != -1
+                            && ids[r + 1][c + 3] == -1
+                            && ids[r + 2][c - 1] == -1
+                            && ids[r + 2][c] == -1
+                            && ids[r + 2][c + 1] != -1
+                            && ids[r + 2][c + 2] == -1
+                            && ids[r + 2][c + 3] == -1
+                            && ids[r + 3][c - 1] == -1
+                            && ids[r + 3][c] == -1
+                            && ids[r + 3][c + 1] == -1
+                            && ids[r + 3][c + 2] == -1
+                            && ids[r + 3][c + 3] == -1;
+
+                    if (boatPattern) {
+                        return new Location(r + 1, c + 1);
+                    }
+                }
+            }
+        }
+
         for (int r = 0; r < grid.getRows(); r++) {
             for (int c = 0; c < grid.getCols(); c++) {
                 if (grid.getCell(r, c) != myIDLocal && grid.getCell(r, c) != -1) {
                     int neighbors = GridFunctions.getNeighbors(r, c, grid);
                     if (neighbors == 3 && GridFunctions.mostCommonNeighbor(r, c, grid) != myIDLocal) {
-                        if (r - 1 >= 0 && r + 3 < grid.getRows()
-                            && c - 1 >= 0 && c + 3 < grid.getCols()) {
-
-                            boolean boatPattern =
-                                // empty border above
-                                ids[r - 1][c - 1] == -1
-                                && ids[r - 1][c] == -1
-                                && ids[r - 1][c + 1] == -1
-                                && ids[r - 1][c + 2] == -1
-                                && ids[r - 1][c + 3] == -1
-
-                                // pattern row 1
-                                && ids[r][c - 1] == -1
-                                && ids[r][c] == -1
-                                && ids[r][c + 1] != -1
-                                && ids[r][c + 2] != -1
-                                && ids[r][c + 3] == -1
-
-                                // pattern row 2
-                                && ids[r + 1][c - 1] == -1
-                                && ids[r + 1][c] != -1
-                                && ids[r + 1][c + 1] == -1
-                                && ids[r + 1][c + 2] != -1
-                                && ids[r + 1][c + 3] == -1
-
-                                // pattern row 3
-                                && ids[r + 2][c - 1] == -1
-                                && ids[r + 2][c] == -1
-                                && ids[r + 2][c + 1] != -1
-                                && ids[r + 2][c + 2] == -1
-                                && ids[r + 2][c + 3] == -1
-
-                                // empty border below
-                                && ids[r + 3][c - 1] == -1
-                                && ids[r + 3][c] == -1
-                                && ids[r + 3][c + 1] == -1
-                                && ids[r + 3][c + 2] == -1
-                                && ids[r + 3][c + 3] == -1;
-
-                            if (boatPattern) {
-                                return new Location(r + 1, c + 1);
-                            }
-                        }
-                        else if (r - 1 >= 0 && r + 2 < grid.getRows() &&
+                        if (r - 1 >= 0 && r + 2 < grid.getRows() &&
                             c - 1 >= 0 && c + 2 < grid.getCols()) {
 
                             boolean stillLife =
@@ -156,6 +152,11 @@ public final class MyAIOffense {
                     }
                 }
             }
+        }
+
+        Location oscillatorMove = new MyAIOscilator(myIDLocal).select(grid);
+        if (oscillatorMove != null) {
+            return oscillatorMove;
         }
 
         return null;
